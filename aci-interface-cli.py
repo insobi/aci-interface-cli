425cd9d9471db8e39eb118bb90f003d342af1aed
Administrator <admin@example.com>
2023-12-05 12:29:33 +0900

    add vpc

--- aci-interface-cli.py
+++ aci-interface-cli.py
@@ -54,6 +54,19 @@ class ACI(object):
         else:
             return False, 'Failed to Execute.'
 
+    def get_vpcIf(self, filters) -> (bool, str):
+        '''call rest api for get VPC interfaces'''
+        url = f'{self.base_url}/api/node/class/vpcIf.json'
+        if filters:
+            url += '?'
+            if filters['descr_exists']:
+                url += 'query-target-filter=ne(vpcIf.descr, "")'
+        response = requests.request("GET", url=url, headers=self.headers, verify=self.ssl_verify)
+        if response.status_code == 200:
+            return True, response.json()
+        else:
+            return False, 'Failed to Execute.'
+
 
 @click.group()
 @click.pass_context
@@ -97,9 +110,9 @@ pod^node^interface^adminSt^mtu^mode^descr
 {{l1PhysIf["dn"].split('/')[1].split('-')[1]}}^{{l1PhysIf["dn"].split('/')[2].split('-')[1]}}^{{l1PhysIf["dn"].split('-')[3][1:-1]}}^{{l1PhysIf.adminSt}}^{{l1PhysIf.mtu}}^{{l1PhysIf.mode}}^{{l1PhysIf.descr}}
 {% endfor %}''')
         rendered = template.render(interfaces=queried)
-        with open('interfaces.csv', 'w') as f:
+        with open('interfaces_phys.csv', 'w') as f:
             f.write(rendered)
-            print('interface.csv file was created.')
+            print('interface_phys.csv file was created.')
     elif raw:
         output = json.dumps(queried)
         click.echo(output)
@@ -122,7 +135,61 @@ pod^node^interface^adminSt^mtu^mode^descr
         click.echo(output)
 
 
+@click.command()
+@click.pass_obj
+@click.option("--csv", default=False, is_flag=True, required=False, help='Downloads as csv file')
+@click.option("--descr-exists", default=False, is_flag=True, required=False, help='Including only interfaces having description')
+@click.option("--raw", default=False, is_flag=True, required=False, help='Shows raw data as json format')
+def vpc(obj, csv, descr_exists, raw):
+    '''Shows physical interfaces'''
+    output = None
+    filters = {}
+    if descr_exists:
+        filters['descr_exists'] = descr_exists
+
+    success, queried = obj.get_vpcIf(filters=filters)
+
+    if not success:
+        click.secho("Execute failed.", fg="red")
+        exit(1)        
+
+    if csv:
+        template = Template(source='''
+sep=^
+pod^node^domain^interface^localOperSt^remoteOperSt^descr
+{% for item in interfaces['imdata'] %}
+{%- set intf = item["vpcIf"]["attributes"] -%}
+{{intf["dn"].split('/')[1].split('-')[1]}}^{{intf["dn"].split('/')[2].split('-')[1]}}^{{intf["dn"].split('/')[6]}}^{{intf["dn"].split('/')[7]}}^{{intf["localOperSt"]}}^{{intf["remoteOperSt"]}}^{{intf["descr"]}}
+{% endfor %}''')
+        rendered = template.render(interfaces=queried)
+        with open('interfaces_vpc.csv', 'w') as f:
+            f.write(rendered)
+            print('interface_vpc.csv file was created.')
+    elif raw:
+        output = json.dumps(queried)
+        click.echo(output)
+    else:
+        output = PrettyTable()
+        output.field_names = ["pod", "node", "domain", "interface", "localOperSt", "remoteOperSt", "descr"]
+        output.align = "l"
+        for item in queried['imdata']:
+            intf = item["vpcIf"]["attributes"]
+            row = [
+                intf["dn"].split('/')[1].split('-')[1],     # pod
+                intf["dn"].split('/')[2].split('-')[1],     # node
+                intf["dn"].split('/')[6],                   # domain
+                intf["dn"].split('/')[7],                   # interface
+                intf["localOperSt"],
+                intf["remoteOperSt"],
+                intf["descr"]
+            ]
+            output.add_row(row)
+        click.echo(output)
+
+
 aci_interface_cli.add_command(phys)
+aci_interface_cli.add_command(vpc)
+
 
 if __name__ == "__main__":
     aci_interface_cli()
